a6515872e84345706332ab2af52837d5da7c95cc
caiocroccia22 <caiocroccia22@gmail.com>
2023-09-26 15:34:48 -0300

    Exercicios

--- 1-print.py
+++ 1-print.py
@@ -3,7 +3,7 @@
 #Para executar o projeto python, vá no diretório do arquivo#
 #digite python nome_arquivo.py#
 
-print("Hello World!")
+# print("Hello World!")
 
 #Python é um linguagem de tipagem dinamica#
 #É uma linguagem de tipagem forte#
@@ -34,19 +34,19 @@ print("Hello World!")
 #Inserindo um comportamento dinamico ao programa#
 #Usuario inseri os valores ao inves de ter ele direto no código#
 
-name = input("Digite o nome do jogo\n")
-#\n - quebra de linha
+# name = input("Digite o nome do jogo\n")
+# #\n - quebra de linha
 
 
-yearLaunch = int(input("Digite o ano de lançamento do jogo\n"))
+# yearLaunch = int(input("Digite o ano de lançamento do jogo\n"))
 
-#Utiliza a função int porque o retorno da função input é sempre em string
-
-
-gamePrice = float(input("Digite o valor do jogo\n"))
+# #Utiliza a função int porque o retorno da função input é sempre em string
 
 
-planeIncluded = bool(input("Está incluido no plano mensal?\n"))
+# gamePrice = float(input("Digite o valor do jogo\n"))
+
+
+# planeIncluded = bool(input("Está incluido no plano mensal?\n"))
 
              
 #Concatenar valores#
@@ -54,25 +54,63 @@ planeIncluded = bool(input("Está incluido no plano mensal?\n"))
 # Alternativa 1
 # print("O ano de lançamento do jogo é", yearLaunch)
 
-#Alternativa 2
-#Um único print para exibir todas as informações juntas
-print("O nome do jogo é:", name, "\n O ano de lançamento do jogo é:", yearLaunch)
+# #Alternativa 2
+# #Um único print para exibir todas as informações juntas
+# print("O nome do jogo é:", name, "\n O ano de lançamento do jogo é:", yearLaunch)
 
-#Alternativa 3
-print(f"O nome do jogo é: {name} \n O ano de lançamento é: {yearLaunch}")
+# #Alternativa 3
+# print(f"O nome do jogo é: {name} \n O ano de lançamento é: {yearLaunch}")
 
 
 #Operadores aritméticos e Relacionais
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o primeiro número: "))
+# num1 = int(input("Digite o primeiro número: "))
+# num2 = int(input("Digite o primeiro número: "))
 
 #Aritmeticos
-sum = num1 + num2
-sub = num1 - num2
-div = num1 / num2
-mult = num1 * num2
+# sum = num1 + num2
+# sub = num1 - num2
+# div = num1 / num2
+# mult = num1 * num2
 
 #Operação para saber o resto da divisão
-mod = num1 % num2
-exp = num1 ** num2
-print(sum, sub, div, mult, mod, exp)
+# mod = num1 % num2
+# exp = num1 ** num2
+# print(sum, sub, div, mult, mod, exp)
+
+#Comparação
+# bigger = num1 > num2
+# smaller = num1 < num2
+# equal = num1 == num2
+# diferrent = num1 != num2
+# bigger_equal = num1 >= num2
+# smaller_equal = num1 <= num2
+
+
+#Atribuição
+
+# num1 = num1 + 1
+# ou num1 += 1
+
+#Exercicio 1
+#Escreve um programa que leia um número e apresente seu antecessor e sucessor
+
+n1 = int(input("Qual número você deseja verificar? "))
+
+suc = n1 + 1
+ant = n1 - 1
+
+print(f"O antecessor do número {n1} é {ant} e o sucessor é {suc}")
+
+#Exercicio 2
+#Escreva um programa em python que leia 4 números e calcule a média entre eles
+
+n2 = int(input("Qual número você deseja verificar a média? "))
+n3 = int(input("Qual número você deseja verificar a média? "))
+n4 = int(input("Qual número você deseja verificar a média? "))
+n5 = int(input("Qual número você deseja verificar a média? "))
+
+med = (n2 + n3 + n4 + n5) / 4
+
+print(f"A média é igual a {med}")
+
+#Detalhando a utilização de Strings
